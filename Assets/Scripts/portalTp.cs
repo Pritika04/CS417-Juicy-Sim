e9b58ef1b6ef79890bf077dec6a3d4fde7216184
Jess Z <52930157+yuan-z123@users.noreply.github.com>
2026-03-31 20:57:36 -0500

    new version

---
+++ Assets/Scripts/portalTp.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using UnityEngine;
+
+public class portalTp : MonoBehaviour
+{
+    public Camera portalCamera;
+    public GameObject thePlayer;
+
+
+    void OnTriggerEnter(Collider other)
+    {
+ 
+            Debug.Log("Trigger hit by: " + other.name);
+            Debug.Log("Teleporting to: " + portalCamera.transform.position);
+            thePlayer.transform.position += portalCamera.transform.position;
+            Debug.Log("Player landed at: " + thePlayer.transform.position);
+
+
+        //Transform playerTransform = player.transform;
+
+
+        //obj.transform.rotation = portalCamera.transform.rotation;
+
+    }
+
+}
